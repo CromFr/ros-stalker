96d4a9459ec8615a761bd622af1aca06a0012e55
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2015-06-19 09:43:48 +0200

    Slower speak & launch camctrl

--- scripts/speak.sh
+++ scripts/speak.sh
@@ -4,4 +4,4 @@ echo $1 |
     V=$(((($RANDOM) % 100) - 50))
     echo -n "<prosody pitch=\"+$V\">$a</prosody> " |
       sed 's/+-/-/' 
-  done | espeak -vfr+f3 -m -p 60 -s 150
+  done | espeak -vfr+f3 -m -p 60 -s 140
